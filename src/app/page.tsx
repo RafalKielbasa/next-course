59cfc39de3f6b26811f836724ca27ac90d6b0051
RafalKielbasa <kielbasarafal92@gmail.com>
2025-02-20 13:17:24 +0100

    feat: chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-export default function Home() {
+import { getServerSession } from 'next-auth'
+import Chat from './components/Chat'
+import { Separator } from '@/components/ui/separator'
+
+export default async function Home() {
+  const session = await getServerSession()
+
+  console.log(session)
+
   return (
     <>
       <h1 className='text-4xl font-bold'>ChatGPT</h1>
       <p>Chat with an AI</p>
+      <Separator />
+      {session && <Chat />}
     </>
   )
 }
